22b37f7d12b216295b526fcc0bba281d496d05cd
devMasrafi <masrafisw@gmail.com>
2026-07-12 22:03:15 +0600

    server: route testing/remember done

--- server/index.js
+++ server/index.js
@@ -4,14 +4,17 @@ const products = require("./products");
 const app = express();
 app.use(express.json());
 
+// GET/Fetch All
 app.get("/", (req, res) => {
   res.send("Welcome to my first Express server!");
 });
 
+// GET/Fetch All the Product
 app.get("/products", (req, res) => {
   res.send(products);
 });
 
+// GET/Fetch single product
 app.get("/products/:id", (req, res) => {
   const product = products.find((product) => {
     return product.id == req.params.id;
@@ -20,7 +23,7 @@ app.get("/products/:id", (req, res) => {
   res.send(product);
 });
 
-// post prodct
+//POST/Create new prodct
 app.post("/products", (req, res) => {
   const newId = () => {
     if (products.length === 0) {
@@ -52,6 +55,54 @@ app.post("/products", (req, res) => {
     res.status(201).json({
       message: " Product Created Successfully",
       newProduct,
+    });
+  }
+});
+
+// PUT/Update product
+app.put("/products/:id", (req, res) => {
+  // Find product
+  const product = products.find((product) => {
+    return product.id == req.params.id;
+  });
+
+  // If not found
+  if (!product || product == null) {
+    return res.status(404).json("update Failed, Product not Found!");
+  }
+
+  // Update name if sent
+  if (req.body.name) {
+    product.name = req.body.name;
+  }
+
+  // Update price if sent
+  if (req.body.price) {
+    product.price = req.body.price;
+  }
+  // Send success response
+  return res.status(200).json({
+    message: "product updated successfull",
+    product,
+  });
+});
+
+// DELETE/remove product
+app.delete("/products/:id", (req, res) => {
+  const index = products.findIndex((product) => {
+    return product.id == req.params.id;
+  });
+
+  if (index === -1) {
+    return res.status(404).json({
+      message: "Product Not Found",
+    });
+  } else {
+    products.splice(index, 1);
+
+    res.status(200).json({
+      message: "Product deleted successfully",
+      products,
     });
   }
 });
