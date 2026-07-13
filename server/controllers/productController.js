a747026f69eb4e9fc9b72f8c0099637a21215511
devMasrafi <masrafisw@gmail.com>
2026-07-13 15:29:24 +0600

    server: Controller added for test

---
+++ server/controllers/productController.js
@@ -0,0 +1,105 @@
+const products = require("../data/products");
+
+// get products
+const getProducts = (req, res) => {
+  res.send(products);
+};
+
+// get Product By using the Id
+const getProductById = (req, res) => {
+  const product = products.find((product) => {
+    return product.id == req.params.id;
+  });
+
+  res.send(product);
+};
+
+// create new product
+const createProduct = (req, res) => {
+  const newId = () => {
+    if (products.length === 0) {
+      return 1;
+    }
+
+    const lastProduct = products[products.length - 1];
+    return lastProduct.id + 1;
+  };
+
+  if (
+    req.body.name == "" ||
+    !req.body.name ||
+    req.body.price == "" ||
+    !req.body.price
+  ) {
+    return res.status(400).json({
+      message: "product has empty field",
+    });
+  }
+
+  const newProduct = {
+    id: newId(),
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  products.push(newProduct);
+
+  res.status(201).json({
+    message: " Product Created Successfully",
+    newProduct,
+  });
+};
+
+// Update a single product
+const updateProduct = (req, res) => {
+  // Find product
+  const product = products.find((product) => {
+    return product.id == req.params.id;
+  });
+
+  // If not found
+  if (!product || product == null) {
+    return res.status(404).json("update Failed, Product not Found!");
+  }
+
+  // Update name if sent
+  if (req.body.name) {
+    product.name = req.body.name;
+  }
+
+  // Update price if sent
+  if (req.body.price) {
+    product.price = req.body.price;
+  }
+  // Send success response
+  return res.status(200).json({
+    message: "product updated successfull",
+    product,
+  });
+};
+
+// delete a product
+const deleteProduct = (req, res) => {
+  const index = products.findIndex((product) => {
+    return product.id == req.params.id;
+  });
+
+  if (index === -1) {
+    return res.status(404).json({
+      message: "Product Not Found",
+    });
+  }
+  products.splice(index, 1);
+
+  res.status(200).json({
+    message: "Product deleted successfully",
+    products,
+  });
+};
+module.exports = {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const products = require("../data/products");
+
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
@@ -8,102 +15,14 @@ router.get("/", (req, res) => {
   res.send("Welcome to my first Express server!");
 });
 
-// GET/Fetch All the Product
-router.get("/products", (req, res) => {
-  res.send(products);
-});
+router.get("/products", getProducts);
 
-// GET/Fetch single product
-router.get("/products/:id", (req, res) => {
-  const product = products.find((product) => {
-    return product.id == req.params.id;
-  });
+router.get("/products/:id", getProductById);
 
-  res.send(product);
-});
+router.post("/products", createProduct);
 
-//POST/Create new prodct
-router.post("/products", (req, res) => {
-  const newId = () => {
-    if (products.length === 0) {
-      return 1;
-    }
+router.put("/products/:id", updateProduct);
 
-    const lastProduct = products[products.length - 1];
-    return lastProduct.id + 1;
-  };
-
-  if (
-    req.body.name == "" ||
-    !req.body.name ||
-    req.body.price == "" ||
-    !req.body.price
-  ) {
-    return res.status(400).json({
-      message: "product has empty field",
-    });
-  }
-  
-  const newProduct = {
-    id: newId(),
-    name: req.body.name,
-    price: req.body.price,
-  };
-
-  products.push(newProduct);
-
-  res.status(201).json({
-    message: " Product Created Successfully",
-    newProduct,
-  });
-});
-
-// PUT/Update product
-router.put("/products/:id", (req, res) => {
-  // Find product
-  const product = products.find((product) => {
-    return product.id == req.params.id;
-  });
-
-  // If not found
-  if (!product || product == null) {
-    return res.status(404).json("update Failed, Product not Found!");
-  }
-
-  // Update name if sent
-  if (req.body.name) {
-    product.name = req.body.name;
-  }
-
-  // Update price if sent
-  if (req.body.price) {
-    product.price = req.body.price;
-  }
-  // Send success response
-  return res.status(200).json({
-    message: "product updated successfull",
-    product,
-  });
-});
-
-// DELETE/remove product
-router.delete("/products/:id", (req, res) => {
-  const index = products.findIndex((product) => {
-    return product.id == req.params.id;
-  });
-
-  if (index === -1) {
-    return res.status(404).json({
-      message: "Product Not Found",
-    });
-  } 
-    products.splice(index, 1);
-
-    res.status(200).json({
-      message: "Product deleted successfully",
-      products,
-    });
-  
-});
+router.delete("/products/:id", deleteProduct);
 
 module.exports = router;
