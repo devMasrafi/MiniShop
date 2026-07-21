7ef133ba5a7e7c80f42377ca06778ca870b8b8bd
devMasrafi <masrafisw@gmail.com>
2026-07-21 13:50:13 +0600

    server: update product model, errorHandler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,4 +1,13 @@
 const errorHandler = (error, req, res, next) => {
+    // console.log(error.name);
+
+    if (error.name === "ValidationError") {
+        return res.status(400).json({
+            success: false,
+            message: error.message,
+        });
+    }
+
     return res.status(500).json({
         success: false,
         message: error.message,
--- server/models/Product.js
+++ server/models/Product.js
@@ -4,13 +4,17 @@ const productSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
+        minlength: 3,
+        trim: true,
     },
     price: {
         type: Number,
         required: true,
+        min: 1,
     },
     description: {
         type: String,
+        trim: true,
     },
     owner: {
         type: mongoose.Schema.Types.ObjectId,
